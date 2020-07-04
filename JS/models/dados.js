fafeeb3acc9892b84e72ca60f007975ec7441778
ThaysMrtns <thays.martins@nave.org.br>
2020-07-03 21:39:13 -0300

    otimização de código

--- JS/models/dados.js
+++ JS/models/dados.js
@@ -1,10 +1,7 @@
-let paragrafo = document.querySelector("#paragrafo");
 
 
 //Let responsável por fazer requisição, que vai pegar os dados de uma API
 let dadosReq = new XMLHttpRequest(); 
-
-let texto = document.createElement("p");
 
 //Abrir a "porta" da nossa requisição 
 dadosReq.open("GET", "https://api.nasa.gov/planetary/apod?api_key=3PmYMPQvIjm5qI4VGgo5eMBADS41PD4fXoal3h4d");
@@ -16,7 +13,7 @@ dadosReq.addEventListener("load", function(){
         let dados = JSON.parse(dadosReq.responseText); //Converti o texto do dadosReq para um objeto json
         console.log(dados);
         //Chamando função afim de exibir o conteúdo da api no meu site
-        exibirSite(dados.copyright, dados.date,dados.hdurl);
+        exibirSite(dados.copyright, dados.date, dados.hdurl);
     }
 })
 //Entrando na porta para pegar os dados
